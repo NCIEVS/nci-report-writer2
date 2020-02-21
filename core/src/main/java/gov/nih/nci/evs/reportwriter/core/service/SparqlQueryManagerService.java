bdb1e7bae00816a645a1e1431a0509901dc87222
Kim Ong <kim.ong@nih.gov>
2020-02-21 03:30:27 -0500

    [EVSREPORT2-42] Associations in Report Writer.

--- core/src/main/java/gov/nih/nci/evs/reportwriter/core/service/SparqlQueryManagerService.java
+++ core/src/main/java/gov/nih/nci/evs/reportwriter/core/service/SparqlQueryManagerService.java
@@ -10,22 +10,25 @@ import gov.nih.nci.evs.reportwriter.core.model.evs.EvsVersionInfo;
 public interface SparqlQueryManagerService {
 
 	public String getEvsConceptLabel(String conceptCode, String namedGraph, String restURL);
-	
+
 	public List <EvsProperty> getEvsProperties(String conceptCode, String namedGraph, String restURL);
-	
+
 	public List<EvsAxiom> getEvsAxioms(String conceptCode, String namedGraph, String restURL);
-	
+
 	public List<EvsConcept> getEvsSubclasses(String conceptCode, String namedGraph, String resutURL);
-	
+
 	public List<EvsConcept> getEvsSuperclasses(String conceptCode, String namedGraph, String restURL);
-	
+
 	public EvsConcept getEvsConceptDetail(String conceptCode, String namedGraph, String restURL);
 
 	public EvsConcept getEvsConceptDetailShort(String conceptCode, String namedGraph, String restURL);
 
 	public List <EvsConcept> getEvsConceptInSubset(String conceptCode, String namedGraph, String restURL);
-	
+
 	public EvsVersionInfo getEvsVersionInfo(String namedGraph, String restURL);
-	
+
 	public List <String> getNamedGraphs(String restURL);
+
+	public List <EvsConcept> getAssociatedEvsConcepts(String conceptCode, String namedGraph, String restURL, String associationName, boolean sourceOf);
+
 }
